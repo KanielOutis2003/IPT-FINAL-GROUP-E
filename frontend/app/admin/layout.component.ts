a9866a289b6cf994aadcf5e8a62a34f43b385e63
Jecu Cutanda <jecucutanda11@gmail.com>
2025-05-18 19:49:13 +0800

    Delete frontend/app directory

--- frontend/app/admin/layout.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-@Component({ 
-    templateUrl: 'layout.component.html',
-    standalone: false
-})
-export class LayoutComponent { }
